996a94d58931d3c698aa370cbbaa1cbb4d11a8e5
CanonManF22 <avi.banerjee164@gmail.com>
2019-04-10 17:13:47 -0700

    connected to firebase

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const firebase = require("firebase");
 const app = express();
+
+
+var config = {
+    apiKey: "AIzaSyCLmKxVPk1O1LKp-r5YTZe-IYKwaXjn00g",
+    authDomain: "hydra-proto.firebaseapp.com",
+    databaseURL: "https://hydra-proto.firebaseio.com",
+    projectId: "hydra-proto",
+    storageBucket: "hydra-proto.appspot.com",
+    messagingSenderId: "58925529561"
+  };
+
+  firebase.initializeApp(config);
+
+var ref = firebase.app().database().ref();
+ref.once('value', ()=> console.log('value'))
+.then(function (snap) {
+console.log(snap.val(), snap.val());
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
 
 app.get('/', (req, res)=>{
     res.send('hello world')
-})
+    
+});
 
 app.get('/HydraData', (req, res)=>{
     console.log('get hydra data');
@@ -33,3 +53,5 @@ app.get('/restartPi', (req, res)=>{
     console.log('restart pi')
     res.send('restart pi');
 });
+
+
